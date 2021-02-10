61a22c2cecafa0e460bbbabc81c276088af495b5
Minkin Stepan <gentostage@gmail.com>
2021-02-10 13:39:52 +0900

    fix model fix max lenght field url and title

--- posts/models.py
+++ posts/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Post (models.Model):
 
-    title = models.CharField("Заголовок", max_length=100)
-    url = models.CharField("URL", max_length=100)
+    title = models.CharField("Заголовок", max_length=250)
+    url = models.CharField("URL", max_length=250)
     created = models.DateTimeField("Дата создания", auto_now=True)
 
 
